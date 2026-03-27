3c41d05132d0fb10f1a02098ff4ae69304a8299a
marvin <m.schuerz@hautzy.com>
2026-03-27 21:15:20 +0100

    it is what it is

---
+++ src/main/kotlin/com/github/minetoblend/ideasearcheverywherefixer/services/SearchEverywhereFixer.kt
@@ -0,0 +1,23 @@
+package com.github.minetoblend.ideasearcheverywherefixer.services
+
+import com.intellij.ide.actions.searcheverywhere.SearchEverywhereFoundElementInfo
+import com.intellij.ide.actions.searcheverywhere.SearchEverywhereReorderingService
+
+@Suppress("UnstableApiUsage")
+class SearchEverywhereFixer : SearchEverywhereReorderingService {
+    override fun isEnabled() = true
+
+    override fun isEnabledInTab(tabID: String) = true
+
+    override fun reorder(
+        tabID: String,
+        items: MutableList<SearchEverywhereFoundElementInfo>
+    ) {
+        items.sortBy {
+            when (it.contributor.searchProviderId) {
+                "ClassSearchEverywhereContributor" -> 0
+                else -> 1
+            }
+        }
+    }
+}
